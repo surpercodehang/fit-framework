6f7ac47ee35bcfb540ecf548debb2076a6759873
CodeCaster <codecaster365@outlook.com>
2025-02-01 22:02:37 +0800

    [fit] 根据文档补充第三个样例代码

---
+++ examples/fit-example/03-dynamic-plugin/plugin-other-weather/src/main/java/modelengine/fit/example/OtherWeather.java
@@ -0,0 +1,25 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *  This file is a part of the ModelEngine Project.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+package modelengine.fit.example;
+
+import modelengine.fitframework.annotation.Component;
+import modelengine.fitframework.annotation.Fitable;
+
+/**
+ * 表示 {@link Weather} 的另一个实现。
+ *
+ * @author 季聿阶
+ * @since 2025-02-01
+ */
+@Component
+public class OtherWeather implements Weather {
+    @Override
+    @Fitable(id = "other")
+    public String get() {
+        return "Other weather plugin is working.";
+    }
+}
