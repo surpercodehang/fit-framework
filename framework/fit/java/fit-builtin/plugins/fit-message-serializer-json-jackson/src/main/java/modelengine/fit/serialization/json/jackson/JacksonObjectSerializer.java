7b1b11ce283f457f6ae4444840bf473cc61567df
猹拿叉攮闰土 <109638984+surpercodehang@users.noreply.github.com>
2025-04-30 14:51:07 +0800

    [fit] 优化序列化器逻辑 (#109)

--- framework/fit/java/fit-builtin/plugins/fit-message-serializer-json-jackson/src/main/java/modelengine/fit/serialization/json/jackson/JacksonObjectSerializer.java
+++ framework/fit/java/fit-builtin/plugins/fit-message-serializer-json-jackson/src/main/java/modelengine/fit/serialization/json/jackson/JacksonObjectSerializer.java
@@ -9,7 +9,6 @@ package modelengine.fit.serialization.json.jackson;
 import static modelengine.fitframework.inspection.Validation.notNull;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.fasterxml.jackson.core.JsonGenerator;
@@ -84,8 +83,7 @@ public class JacksonObjectSerializer implements ObjectSerializer {
                 .maxStringLength(Integer.MAX_VALUE)
                 .build()).build()).configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .setAnnotationIntrospector(new FitAnnotationIntrospector())
-                .setVisibility(visibilityChecker)
-                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+                .setVisibility(visibilityChecker);
         SimpleModule module = new SimpleModule();
         module.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormat));
         module.addDeserializer(LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormat));
