bb5b10882b7c294afd258d22db4cbbf90c63a5da
CodeCaster <codecaster365@outlook.com>
2025-08-29 17:03:07 +0800

    Release 3.5.1

--- framework/fel/java/plugins/tool-mcp-server/src/test/java/modelengine/fel/tool/mcp/server/support/DefaultMcpServerTest.java
+++ framework/fel/java/plugins/tool-mcp-server/src/test/java/modelengine/fel/tool/mcp/server/support/DefaultMcpServerTest.java
@@ -75,7 +75,7 @@ public class DefaultMcpServerTest {
 
             ServerSchema.Info serverInfo = info.serverInfo();
             assertThat(serverInfo).returns("FIT Store MCP Server", ServerSchema.Info::name)
-                    .returns("3.5.1-SNAPSHOT", ServerSchema.Info::version);
+                    .returns("3.5.1", ServerSchema.Info::version);
         }
     }
 
