ad6c2592418e66b065d745ba22029ecd3dc772b7
song <271667068@qq.com>
2025-09-18 20:31:08 +0800

    [waterflow] supports configuring uuid generator (#299)

--- framework/waterflow/java/waterflow-core/src/main/java/modelengine/fit/waterflow/domain/utils/UUIDUtil.java
+++ framework/waterflow/java/waterflow-core/src/main/java/modelengine/fit/waterflow/domain/utils/UUIDUtil.java
@@ -6,7 +6,10 @@
 
 package modelengine.fit.waterflow.domain.utils;
 
+import modelengine.fitframework.inspection.Validation;
+
 import java.util.UUID;
+import java.util.function.Supplier;
 
 /**
  * Uuid的Utils类。
@@ -15,12 +18,27 @@ import java.util.UUID;
  * @since 1.0
  */
 public class UUIDUtil {
+    private static Supplier<String> uuidGenerator = () -> UUID.randomUUID().toString().replace("-", "");
+
     /**
      * 随机生成uuid。
      *
      * @return 随机生成的uuid的 {@link String}。
      */
     public static String uuid() {
-        return UUID.randomUUID().toString().replace("-", "");
+        return uuidGenerator.get();
+    }
+
+    /**
+     * 全局替换 uuid 的生成器。
+     *
+     * @param uuidGenerator 表示要设置的 uuid 生成器的 {@link Supplier}{@code <}{@link String}{@code >}。
+     * @return 表示设置前使用的 uuid 生成器的 {@link Supplier}{@code <}{@link String}{@code >}。
+     */
+    public static synchronized Supplier<String> setUuidGenerator(Supplier<String> uuidGenerator) {
+        Validation.notNull(uuidGenerator, "The uuid generator should not be null.");
+        Supplier<String> oldUuidGenerator = UUIDUtil.uuidGenerator;
+        UUIDUtil.uuidGenerator = uuidGenerator;
+        return oldUuidGenerator;
     }
 }
